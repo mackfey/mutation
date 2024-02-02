fc5c6715cb3f2e57f5b7d37d792146517d170010
Michael Ernst <mernst@cs.washington.edu>
2024-02-02 16:35:04 +0000

    Merged in doc (pull request #3)
    Approved-by: Rene Just

--- show_mutant.sh
+++ show_mutant.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-git diff --no-index -W src/triangle/Triangle.java .mutated/mutants/$1/triangle/Triangle.java
+# Invoke as
+#   show_mutant NUMBER
+# For example:
+#   show_mutant 45
+
+git diff --no-index -W src/triangle/Triangle.java .mutated/mutants/"$1"/triangle/Triangle.java
